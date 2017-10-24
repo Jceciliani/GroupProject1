0fd6f8fdc9cdf009f945a0bc532604e499de2b46
irlbeckt <33010335+irlbeckt@users.noreply.github.com>
2017-10-24 18:04:55 -0400

    Add files via upload

--- GroupProject1/Tool.cpp
+++ GroupProject1/Tool.cpp
@@ -1,10 +1,98 @@
+/******************************************************************************
+** Authors: Devon Aleshire, Joshua Ceciliani, Todd Irlbeck, Nikita Mattingly,
+** and Edward Pietryk
+** Date: 10/22/2017
+** Description: Tool.cpp is the Tool class definition file.
+** The Tool class has member variables including Strength and Type, and a 
+** member function called SetStrength(int) that sets the strength for the Tool.
+**
+** References:
+** TODO
+******************************************************************************/
+
 #include "Tool.h"
 
-Tool::Tool(){ }
-
-void Tool::setStrength(const int strength)
+/******************************************************************************
+*                               Tool::Tool()
+* Description: Default constructor
+******************************************************************************/
+Tool::Tool()
 {
-	this->strength = strength;
+    strength = -1; //Strength initialized to -1 to indicate need for setting
+    type = 63;     //Type is initialized to '?' to indicate need for setting
 }
 
-Tool::~Tool() { }
+/******************************************************************************
+*                               Tool::Tool()
+* Description: Overload constructor
+* Parameters: 
+*    int, tool strength
+*    char, tool type
+* Return type: 
+*    None
+******************************************************************************/
+Tool::Tool(int strength, char type)
+{
+    this->strength = strength;
+    this->type = type;
+}
+
+/******************************************************************************
+*                               Tool::~Tool()
+* Description: Destructor
+******************************************************************************/
+Tool::~Tool()
+{
+}
+
+/******************************************************************************
+*                             Tool::setStrength()
+* Description: Sets the strength for tool
+* Parameters: 
+*    int, tool strength
+* Return type: 
+*    None
+******************************************************************************/
+void Tool::setStrength(const int strength)
+{
+    this->strength = strength;
+}
+
+/******************************************************************************
+*                             Tool::getStrength()
+* Description: Gets the strength that tool is set to
+* Parameters: 
+*    None
+* Return type: 
+*    int, tool strength
+******************************************************************************/
+int Tool::getStrength() const
+{
+    return strength;
+}
+
+/******************************************************************************
+*                             Tool::setType()
+* Description: Sets the type for tool
+* Parameters: 
+*    char, tool type
+* Return type: 
+*    None
+******************************************************************************/
+void Tool::setType(const char type)
+{
+    this->type = type;
+}
+
+/******************************************************************************
+*                             Tool::getStrength()
+* Description: Gets the tool type
+* Parameters: 
+*    None
+* Return type: 
+*    char, tool type
+******************************************************************************/
+char Tool::getType() const
+{
+    return type;
+}
--- GroupProject1/Tool.h
+++ GroupProject1/Tool.h
@@ -1,19 +1,28 @@
+/******************************************************************************
+** Authors: Devon Aleshire, Joshua Ceciliani, Todd Irlbeck, Nikita Mattingly,
+** and Edward Pietryk
+** Date: 10/22/2017
+** Description: Tool.h is the Tool class header file.
+** The Tool class has member variables including Strength and Type, and a 
+** member function called SetStrength(int) that sets the strength for the Tool.
+******************************************************************************/
+
 #ifndef TOOL_H
 #define TOOL_H
 
 class Tool
 {
-	protected:
-		int strength;
-		char type;
-
-	public:
-		Tool();
-		virtual void setStrength(const int);
-		virtual ~Tool();
+        protected:
+                int strength;
+                char type;
+        public:
+                Tool();
+                Tool(int, char);
+                virtual ~Tool();
+                virtual void setStrength(const int);
+                virtual int getStrength() const;
+                virtual void setType(const char);
+                virtual char getType() const;
 };
 
 #endif // !TOOL_H
-
-
-
