1e5a56e497d22c70d98f3b59462a6476ddbf628c
nikmatti <nikita.mattingly@gmail.com>
2017-10-24 15:17:10 -0700

    Update Rock.h

--- GroupProject1/Rock.h
+++ GroupProject1/Rock.h
@@ -1,3 +1,4 @@
+/***************
 #ifndef ROCK_H
 #define ROCK_H
 #include "Tool.h"
@@ -20,5 +21,37 @@ class Rock : public Tool
 };
 
 #endif // !ROCK_H
+*********************/
+
+#ifndef RPSGame_HPP
+#define RPSGame_HPP
+#include <cstdio> // Reference: https://www.tutorialspoint.com/c_standard_library/stdio_h.htm
+#include "tool.h"
+
+class RPSGame 
+{
+protected:
+	Tool *human;
+	Tool *computer;
+	Tool* getTool();
+	Tool* getComputer();
+	int human_wins;
+	int computer_wins;
+	int ties;
+	int customRockStrength();
+	int customPaperStrength();
+	int customScissorsStrength();
+	void checkFight();
+	void chooseStrength();
 
 
+public:
+	RPSGame();
+	int playRPS();
+	void operator=(const RPSGame &other);
+	RPSGame(const RPSGame &other);
+	~RPSGame();
+
+
+};
+#endif
