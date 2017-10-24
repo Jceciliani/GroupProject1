1546959d10bf60b017e8326d00875c9581abdf1a
nikmatti <nikita.mattingly@gmail.com>
2017-10-24 15:19:44 -0700

    Update RPSGame.h

--- GroupProject1/RPSGame.h
+++ GroupProject1/RPSGame.h
@@ -1,3 +1,4 @@
+/**************************************************
 #ifndef RPSGAME_H
 #define RPSGAME_H
 #include "Tool.h"
@@ -20,5 +21,39 @@ class RPSGame
 
 
 #endif // !RPSGAME_H
+************************************************/
+
+#ifndef RPSGame_HPP
+#define RPSGame_HPP
+#include <cstdio> // Reference: https://www.tutorialspoint.com/c_standard_library/stdio_h.htm
+#include "tool.h"
+
+class RPSGame 
+{
+protected:
+	Tool *human;
+	Tool *computer;
+	Tool* getTool();
+	Tool* getComputer();
+	int human_wins;
+	int computer_wins;
+	int ties;
+	int customRockStrength();
+	int customPaperStrength();
+	int customScissorsStrength();
+	void checkFight();
+	void chooseStrength();
+
+
+public:
+	RPSGame();
+	int playRPS();
+	void operator=(const RPSGame &other);
+	RPSGame(const RPSGame &other);
+	~RPSGame();
+
+
+};
+#endif
 
 
