591f3926a0fba46af018374ca067974f247ea439
Jceciliani <jceciliani@gmail.com>
2017-10-26 23:58:07 -0600

    Update play_game.cpp

--- GroupProject1/play_game.cpp
+++ GroupProject1/play_game.cpp
@@ -4,8 +4,17 @@
 
 int main()
 {
-    RPSGame game;
-    game.playRPS();
+    //RPSGame game;
+    //game.playRPS();
+    //return 0;
+    
+    RPSGame *game = new RPSGame();
+    game->playRPS();
+    delete game;
+    
     return 0;
+    
+    
+    
 }
 
