2ee8616d8e4e16a48cf4153cd53738a50ffbb6de
Jceciliani <jceciliani@gmail.com>
2017-10-27 17:41:17 -0600

    Update Scissors.cpp

--- GroupProject1/Scissors.cpp
+++ GroupProject1/Scissors.cpp
@@ -12,4 +12,23 @@ Scissors::Scissors(int strength)
 	setStrength(strength);
 }
 
+int Scissors::fight(Tool* tool)
+{
+	int fightOutcome = 0;
+	int strength = this->getStrength();
+	
+	if (tool->getType() == PAPER)
+	{
+		strength *= 2;
+	}
+	else if (tool->getType() == ROCK)
+	{
+		strength *= .5;
+	}
+	Tool::DisplayBattleStrengths(strength, tool->getStrength());
+	fightOutcome = Tool::compare(strength, tool->getStrength());
+
+	return fightOutcome;
+}
+
 Scissors::~Scissors() { }
